65329b271f84d8cb4f14bf6f339384ae351ec8e6
Evelyn Maria de Lima <DUQ0295830@aluno.firjansenaisesi.com.br>
2025-07-25 20:55:09 -0300

    pronto

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import styles from "./footer.module.css";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.content}>
-        &copy; {new Date().getFullYear()} E-commerce Admin Panel
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <p className={styles.dir}>
+            &copy; {new Date().getFullYear()} E-commerce Admin Panel
+          </p>
+          <div className={styles.links}>
+            <a
+              className={styles.githubLink}
+              href="https://github.com/Bernardo-cmm/02"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <BsGithub />
+              Autores: Evelyn / Bianca / Bernardo
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
